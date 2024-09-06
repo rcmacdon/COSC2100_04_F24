e5a1b41510a82858cf74dc62311e88b431b43a8d
rcmacdon <clint.macdonald@durhamcollege.ca>
2024-09-06 16:10:30 -0400

    Pushing Demo 011

---
+++ COSC2100_04_Demos/Demo01/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Demo01
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(210)))), ((int)(((byte)(0)))));
+            this.btnHelloWorld.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelloWorld.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnHelloWorld.Location = new System.Drawing.Point(244, 129);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(294, 151);
+            this.btnHelloWorld.TabIndex = 0;
+            this.btnHelloWorld.Text = "Hello World";
+            this.btnHelloWorld.UseVisualStyleBackColor = false;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHelloWorld);
+            this.Name = "frmMainForm";
+            this.Text = "COSC1200 Demo 2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHelloWorld;
+    }
+}
+
---
+++ COSC2100_04_Demos/Demo01/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo01
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloWorld_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Hello World", "Clint was Here", MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+            {
+                btnHelloWorld.Text = "Yeah!";
+            }
+
+        }
+    }
+}
