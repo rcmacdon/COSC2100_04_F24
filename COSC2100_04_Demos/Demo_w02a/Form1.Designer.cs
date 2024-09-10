0ab7c686bb5615c40bc9376e6891feee99d31872
rcmacdon <clint.macdonald@durhamcollege.ca>
2024-09-10 13:58:18 -0400

    Finished Week 02 Class 1

--- COSC2100_04_Demos/Demo_w02a/Form1.Designer.cs
+++ COSC2100_04_Demos/Demo_w02a/Form1.Designer.cs
@@ -28,21 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblMainTitle = new System.Windows.Forms.Label();
+            this.lbxElves = new System.Windows.Forms.ListBox();
+            this.lblElfList = new System.Windows.Forms.Label();
+            this.txtElfName = new System.Windows.Forms.TextBox();
+            this.lblElfName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUnselect = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblMainTitle
+            // 
+            this.lblMainTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMainTitle.BackColor = System.Drawing.Color.Black;
+            this.lblMainTitle.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMainTitle.ForeColor = System.Drawing.Color.White;
+            this.lblMainTitle.Location = new System.Drawing.Point(0, 1);
+            this.lblMainTitle.Name = "lblMainTitle";
+            this.lblMainTitle.Size = new System.Drawing.Size(460, 35);
+            this.lblMainTitle.TabIndex = 0;
+            this.lblMainTitle.Text = "My Friendly Elves";
+            this.lblMainTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxElves
+            // 
+            this.lbxElves.FormattingEnabled = true;
+            this.lbxElves.ItemHeight = 20;
+            this.lbxElves.Location = new System.Drawing.Point(6, 82);
+            this.lbxElves.Name = "lbxElves";
+            this.lbxElves.Size = new System.Drawing.Size(169, 184);
+            this.lbxElves.TabIndex = 3;
+            this.lbxElves.SelectedIndexChanged += new System.EventHandler(this.lbxElves_SelectedIndexChanged);
+            // 
+            // lblElfList
+            // 
+            this.lblElfList.AutoSize = true;
+            this.lblElfList.Location = new System.Drawing.Point(9, 51);
+            this.lblElfList.Name = "lblElfList";
+            this.lblElfList.Size = new System.Drawing.Size(94, 20);
+            this.lblElfList.TabIndex = 2;
+            this.lblElfList.Text = "List of Elves";
+            // 
+            // txtElfName
+            // 
+            this.txtElfName.Location = new System.Drawing.Point(347, 79);
+            this.txtElfName.Name = "txtElfName";
+            this.txtElfName.Size = new System.Drawing.Size(100, 26);
+            this.txtElfName.TabIndex = 0;
+            // 
+            // lblElfName
+            // 
+            this.lblElfName.AutoSize = true;
+            this.lblElfName.Location = new System.Drawing.Point(230, 82);
+            this.lblElfName.Name = "lblElfName";
+            this.lblElfName.Size = new System.Drawing.Size(111, 30);
+            this.lblElfName.TabIndex = 4;
+            this.lblElfName.Text = "Elf Name";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(371, 225);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(290, 225);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 40);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUnselect
+            // 
+            this.btnUnselect.Location = new System.Drawing.Point(108, 42);
+            this.btnUnselect.Name = "btnUnselect";
+            this.btnUnselect.Size = new System.Drawing.Size(92, 37);
+            this.btnUnselect.TabIndex = 5;
+            this.btnUnselect.Text = "Unselect";
+            this.btnUnselect.UseVisualStyleBackColor = true;
+            this.btnUnselect.Click += new System.EventHandler(this.btnUnselect_Click);
             // 
             // frmMain
             // 
+            this.AcceptButton = this.btnSave;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Blue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(458, 277);
+            this.Controls.Add(this.btnUnselect);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblElfName);
+            this.Controls.Add(this.txtElfName);
+            this.Controls.Add(this.lblElfList);
+            this.Controls.Add(this.lbxElves);
+            this.Controls.Add(this.lblMainTitle);
+            this.MinimumSize = new System.Drawing.Size(480, 333);
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Week 02 Demo A";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMainTitle;
+        private System.Windows.Forms.ListBox lbxElves;
+        private System.Windows.Forms.Label lblElfList;
+        private System.Windows.Forms.TextBox txtElfName;
+        private System.Windows.Forms.Label lblElfName;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUnselect;
     }
 }
 
--- COSC2100_04_Demos/Demo_w02a/Form1.cs
+++ COSC2100_04_Demos/Demo_w02a/Form1.cs
@@ -16,5 +16,40 @@ namespace Demo_w02a
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes) Application.Exit();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtElfName.Text.Trim().Length > 0)
+            {
+                lbxElves.Items.Add(txtElfName.Text.Trim());
+                txtElfName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("The Elf Name is Required!");
+            }
+        }
+
+        private void lbxElves_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = lbxElves.SelectedIndex;
+            if (selectedIndex > -1)
+            {
+                String selectedString = lbxElves.Items[selectedIndex].ToString();
+                txtElfName.Text = selectedString;
+            }
+        }
+
+        private void btnUnselect_Click(object sender, EventArgs e)
+        {
+            lbxElves.SelectedItems.Clear();
+            txtElfName.Clear();
+        }
     }
 }
